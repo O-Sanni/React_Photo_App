9079144186f4bf5bd3ef0c86f1cac40da06ba18f
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-18 13:48:14 -0400

    LogIn_SignUp.js

---
+++ src/components/LogIn_SignUp.js
@@ -0,0 +1,18 @@
+import Redux from "redux";
+
+class LogIn_SignUp extends React_Compoent{
+    constructor(props){
+        super(props);
+        this.state={
+            login:null,
+            password: null
+        }
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}

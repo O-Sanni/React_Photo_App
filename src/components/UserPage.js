7d23f45a51fa2ac2c99cafae04a4639eac219d78
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-19 19:58:41 -0400

    user page working data rendering and update db.json

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,3 +1,11 @@
+// the props.id will be send to UserPage to open page for the particular user;
+//there will be async function to get information by using axios.get
+// will get information from http://my-json-server.typicode.com/OlgaSannikov/React_Photo_App/users/ and /images ? userId= to userid
+//then create check function to make sure that user info and images not empty
+// then if it is not empty  function will return user information and image
+//to render images i will use .map()
+//create function to get time stamp for each image
+
 import React from "react";
 import axios from "axios";
 
@@ -5,7 +13,6 @@ class UserPage extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            userId: props.id,
             user:"",
             images:""
         }
@@ -14,46 +21,27 @@ class UserPage extends React.Component{
 async getUserInfo(){
         try{
             const listOfUserInfo= await axios.get(`http://my-json-server.typicode.com/OlgaSannikov/React_Photo_App/users/${this.props.id}`);
-            // const listOfUserImages=await axios.get(`http://my-json-server.typicode.com/OlgaSannikov/React_Photo_App/images?userid=${this.state.userId}`);
+            const listOfUserImages=await axios.get(`http://my-json-server.typicode.com/OlgaSannikov/React_Photo_App/images?userid=${this.props.id}`);
             this.setState({user: listOfUserInfo.data});
-            // this.setState({images: listOfUserImages.data})
-            // let img=this.state.images.map((res)=>{
-            //     return res.imgurl;
-            // })
-            // console.log(img);
-            // console.log(this.state.images[0].imgurl);
+            this.setState({images: listOfUserImages.data});
         }
         catch(error){
             console.log(error);
         }
     }
-    // async getUserImages(){
-    //     try{
-    //         const listOfUserImages=await axios.get(`http://my-json-server.typicode.com/OlgaSannikov/React_Photo_App/images?userid=${this.state.userId}`); 
-    //         this.setState({images: listOfUserImages.data})
-
-    //     }
-    //     catch(error){
-    //         console.log(error);
-    //     }
-    // }
     componentDidMount(){
-        this.getUserInfo();
-        //this.getUserImages();
-        // console.log(this.state.images)
-        axios.get(`http://my-json-server.typicode.com/OlgaSannikov/React_Photo_App/images?userid=${this.props.id}`).
-        then(res=>
-            {this.setState({images: res.data})})
-
-        .catch((error)=>{
-            console.log(error);
-        })
-
+        this.getUserInfo();   
     }
-    // getTimeStamp(){
-    //     let timeStamp=new Date();
-    //     return timeStamp.Date.now();
-    // }
+    getTimeStamp(){
+        const month=["Jan","Feb","Apr","M"]
+        let timeStamp=Date.now();
+
+
+
+
+
+        return timeStamp;
+    }
 //     getImages(){
 //         console.log(this.state.images[0].id);
 
@@ -74,21 +62,15 @@ async getUserInfo(){
             return "Sorry, information is not availiable or user page has been deleted.";
     }
     else{
-        
+        let images=this.state.images.map(res=>
+            {
+                console.log(res.id)
+                console.log(this.getTimeStamp())
+            })
         // return (<div>
                     
-                     console.log(this.state.user.photo)
-                    console.log(this.state.images[0])
-                    return <p>{this.state.images[0].id}</p>
-        //             {/* <img src={this.state.images[0].imgurl} alt="profile photo" /> */}
-        //             {/* <p>Username</p>
-        //             <h4>{this.state.wineList.year}</h4>
-        //             <p>Date of Birth</p> 
-        //             <h4>{this.state.wineList.grapes}</h4>
-        //             <p>E-mail</p>
-        //             <h4>{this.state.wineList.country}</h4> */}
-        //             {/* {this.getImages()} */}
-        //             </div>)
+             
+        //         </div>)
     }
     }
     
@@ -96,7 +78,6 @@ async getUserInfo(){
             
             return (<div>
             {this.checkIfUserExists()}
-            {/* {console.log(this.state.images[0])} */}
             </div> 
             );
         }

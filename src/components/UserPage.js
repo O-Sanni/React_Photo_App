37e7444dfb9bcbf6917ea75ebd70bf4cda84f7cf
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-18 23:41:33 -0400

    adding timeStamp to UserPage

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -6,7 +6,7 @@ class UserPage extends React.Component{
         super(props);
         this.state={
             userId: props.id,
-            user:null,
+            user:"",
         }
         
     }
@@ -14,7 +14,8 @@ async getUserList(){
         try{
             const listOfUsers= await axios.get(`http://my-json-server.typicode.com/OlgaSannikov/React_Photo_App/users/${this.state.userId}`)
             this.setState({user: listOfUsers.data});
-            console.log(this.state.user)
+            console.log(this.state.user);
+            console.log(this.state.user.photo);
         }
         catch(error){
             console.log(error);
@@ -28,38 +29,38 @@ async getUserList(){
         return timeStamp.Date.now();
     }
     getImages(){
-        console.log(this.state.user.photo)
-        let images=this.state.user.map((response,index)=>{
+        // console.log(this.state.user.photo)
+        let images=this.state.user.image.map((response,index)=>{
             return (<div id={index}>
-             <img src={response.image} alt="image" />
-             <p>{this.getTimeStamp()}</p>
+             <img src={response} alt="image" />
+             {/* <p>{this.getTimeStamp()}</p> */}
              </div> )});
 
              return images;
     }
     
-    // checkIfUserExists(){
-    //     if(this.state.userId===null){
-    //         return "Sorry, information is not availiable or user page has been deleted.";
-    // }
-    // else{
-    //     return (<div>
-    //                 {/* {console.log(this.state.user.photo)} */}
-    //                 {/* <img src={this.state.user.photo} alt="profile photo" /> */}
-    //                 {/* <p>Username</p>
-    //                 <h4>{this.state.wineList.year}</h4>
-    //                 <p>Date of Birth</p> 
-    //                 <h4>{this.state.wineList.grapes}</h4>
-    //                 <p>E-mail</p>
-    //                 <h4>{this.state.wineList.country}</h4> */}
-    //                 {this.getImages()}
-    //                 </div>)
-    // }
-    // }
+    checkIfUserExists(){
+        if(this.state.userId===null){
+            return "Sorry, information is not availiable or user page has been deleted.";
+    }
+    else{
+        return (<div>
+                    {console.log(this.state.user.photo)}
+                    <img src={this.state.user.photo} alt="profile photo" />
+                    {/* <p>Username</p>
+                    <h4>{this.state.wineList.year}</h4>
+                    <p>Date of Birth</p> 
+                    <h4>{this.state.wineList.grapes}</h4>
+                    <p>E-mail</p>
+                    <h4>{this.state.wineList.country}</h4> */}
+                    {this.getImages()}
+                    </div>)
+    }
+    }
         render(){   
     
             return (<div>
-            {/* {this.checkIfUserExists()} */}
+            {this.checkIfUserExists()}
             </div> 
             );
         }
